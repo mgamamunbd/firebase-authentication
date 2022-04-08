a404a535bd68aea45c869f756346d35dae2d4f91
mgamamunbd <mgamamunbd@gmail.com>
2022-04-09 00:46:46 +0400

    updated 1246

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import React from "react";
 import "./App.css";
 import app, { getAuth } from "./firebase.init";
 
@@ -7,7 +8,21 @@ app;
 const auth = getAuth();
 
 function App() {
-  return <div className='App'></div>;
+  const [user, setUser] = React.useState({});
+  const provider = new auth.GoogleAuthProvider();
+  
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+  });
+  return (
+    <div className='App'>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+    </div>
+  );
 }
 
 export default App;
