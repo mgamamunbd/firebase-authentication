75485c348a175185eed0498911379a273d9a03bb
mgamamunbd <mgamamunbd@gmail.com>
2022-04-09 09:12:19 +0400

    some issue fixed

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@
 
 import React from "react";
 import "./App.css";
-import app, { getAuth } from "./firebase.init";
-
-app;
-const auth = getAuth();
 
 function App() {
-  const [user, setUser] = React.useState({});
-  const provider = new auth.GoogleAuthProvider();
-  
-  const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
-    .then(result => {
-      const user = result.user;
-      setUser(user);
-      console.log(user);
-  });
-  return (
-    <div className='App'>
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
