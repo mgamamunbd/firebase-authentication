cd85010ebdcb2e98b93b4807c9a8a9eb242a9f8f
mgamamunbd <mgamamunbd@gmail.com>
2022-04-10 11:01:43 +0400

    fixed some issue

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
 /** @format */
 
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import React from "react";
-import "./App.css";
-import { getAuth } from "./firebase/auth";
-const auth = getAuth();
-
-function App() {
-  const handleGoogleSignIn = () => {
-    console.log("working");
-=======
-import { getAuth, GoogleAuthProvided, signInWithPopup } from "firebase/auth";
-import React, { useState } from "react";
 import "./App.css";
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
-  const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvided();
-
+  const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-        console.log(result.user);
-      })
-      .catch((error) => {
-        console.error("error", error);
-      });
->>>>>>> 3576959a20b6756c2df0896265d3d8688b2293ad
+    console.log("working");
   };
   return (
     <div className='App'>
       <h1>Firebase Authentication</h1>
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button className='btn btn-info' onClick={handleGoogleSignIn}>
+        Google Sign In
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 /** @format */
 
 import React from "react";
-<<<<<<< HEAD
-import { createRoot } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-=======
 import { createRoot } from "react-dom/client";
->>>>>>> 3576959a20b6756c2df0896265d3d8688b2293ad
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -14,17 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-<<<<<<< HEAD
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
-=======
   <React.StrictMode>
     <App />
   </React.StrictMode>
->>>>>>> 3576959a20b6756c2df0896265d3d8688b2293ad
 );
 
 // If you want to start measuring performance in your app, pass a function
